57b0b2c4bf4c15a93ac0dfbf60ba3950a86bdb16
Janaina Oliveira <janainaon1@gmail.com>
2022-09-08 09:59:13 -0300

    add _variaveis.scss

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -10,6 +10,11 @@ export default function Cardapio() {
         {/* <img src={logo} alt="Logo do Aluroni" /> */}
         <Logo />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
+      </header>
     </main>
   )
 }
