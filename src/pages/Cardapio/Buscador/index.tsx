18140ebf9c880e484e05e85f4d69bc0e99e6ac77
Janaina Oliveira <janainaon1@gmail.com>
2022-09-08 10:49:37 -0300

    add favicon, placeholder

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -13,6 +13,7 @@ export default function Buscador({ busca, setBusca }: Props) {
       <input 
         value={busca} 
         onChange={(event) => setBusca(event.target.value)} 
+        placeholder="Buscar..."
       />
       <CgSearch size={20} color='#4C4D5E' />
     </div>
