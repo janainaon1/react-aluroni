4cbf36d2d8bf386b50eb1149c42e937e4954462c
Janaina Oliveira <janainaon1@gmail.com>
2022-11-14 09:39:59 -0300

    feat: implements itens

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
-import styles from './Cardapio.module.scss';
+import { useState } from "react";
+import styles from "./Cardapio.module.scss";
 
 // import logo from '../../assets/logo.svg';
-import { ReactComponent as Logo } from 'assets/logo.svg';
-import Buscador from './Buscador';
-import Filtros from './Filtros';
-import Ordenador from './Ordenador';
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./Buscador";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
+import Itens from "./Itens";
 
 export default function Cardapio() {
-  const [busca, setBusca] = useState('');
+  const [busca, setBusca] = useState("");
   const [filtro, setFiltro] = useState<number | null>(null);
-  const [ordenador, setOrdenador] = useState('');
+  const [ordenador, setOrdenador] = useState("");
 
   return (
     <main>
@@ -19,18 +20,17 @@ export default function Cardapio() {
         <Logo />
       </nav>
       <header className={styles.header}>
-        <div className={styles.header__text}>
-          A casa do código e da massa
-        </div>
+        <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
           <Filtros filtro={filtro} setFiltro={setFiltro} />
-          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}  />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens />
       </section>
     </main>
-  )
+  );
 }
